1298735bf118c6c8d29da2699f1c1bb67430b311
Ben Doyle <benjamin.peter.doyle@gmail.com>
2014-01-31 11:40:06 -0500

    Add a StateT (Snap r) layer to the render component.
    Previously we assembled a type (r -> Splices s). This probably bound
    the splices to names too late (at runtime), and seemed to require
    a viscious hack to get to compile.

--- src/Snap/Crud.hs
+++ src/Snap/Crud.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Snap.Crud where
 
 import           Snap
@@ -24,42 +25,30 @@ import qualified Database.Groundhog as GH
 --       for the parsing side).
 --     - update a field in a record.
 --     - delete a record.
-type CRUD r a = Product (Recieve) (Render r) a
 
-field :: (Read a, Show a) => ByteString -> Lens' r a -> a -> Field r a
-field name l v = Pair recieve render
+type Crud r a = Product (Recieve) (Render r) a
+
+field :: (Read a, Show a) => ByteString -> Lens' r a -> Crud r a
+field name l = Pair recieve render
   where
     -- We really want a user-supplied parser, and error-handling
     -- for the no-such-parameter case
     recieve = getParam name >>= maybe pass (return . read . B.unpack)
-    
-    splice = return . yieldRuntime . return
-           . textSplice (T.pack . show) . view l
-    render = const undefined <$> tell (\r -> T.decodeUtf8 name ## splice r)
-    
-    -- If we're rendering (a single record, not a list),
-    -- the last segment of the path represents the record id
---     render = Const (T.decodeUtf8 name ## splice)
---     splice = return . yieldRuntime $ do
---         key <- lift rqPathInfo
---         val <- view l <$> lift (GH.get key)
---         return $ textSplice (T.pack . show) val
---     render = ContT $ \fromKey -> do
---         key <- lift $ withRequest (return . rqPathInfo)
---         rec <- fromKey key
---         let splice = return . yieldRuntime . return
---                    . textSplice (T.pack . show)
---                    $ view l rec
---         tell (T.decodeUtf8 name ## splice)
---         return rec
-
+   
+    --splice :: Snap r -> Splice Snap
+    splice m = return . yieldRuntime $
+           lift m >>= return . textSplice (T.pack . show) . view l
+    render = do
+       m   <- lift get
+       tell $ (T.decodeUtf8 name ## splice m)
+       rec <- lift . lift $ m
+       lift . put $ return rec
+       return $ view l rec
 
 type Recieve = Snap 
 
---type Render r = ContT r (WriterT (Splices (Splice Snap))  Snap)
-type Render r = Writer (r -> Splices (Splice Snap))
+type Render r = WriterT (Splices (Splice Snap)) (StateT (Snap r) Snap)
+    
 
---type Writer mo = Product (Const mo) Identity
-
---tell :: Monoid mo => mo -> Writer mo ()
---tell mo = Pair (Const mo) (pure ())
+    -- If we're rendering (a single record, not a list),
+    -- the last segment of the path represents the record id
